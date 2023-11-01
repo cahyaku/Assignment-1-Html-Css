7d2683330e0b0203088441e90d051a190f98a3d6
Cahya <cahyakumala573@gmail.com>
2023-11-01 16:37:53 +0800

    Continue create responsive mobile

---
+++ Latihan-1/js/slider.js
@@ -0,0 +1,13 @@
+// new Splide(".splide").mount();
+
+var splide = new Splide(".splide", {
+  width: "100%",
+});
+
+splide.mount();
+
+// .splide__arrow {}
+// new Splide(".splide", {
+//   type: "loop",
+//   padding: "20%",
+// });
