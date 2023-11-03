bf2d91be8cb2d7e87459216a7a2855c7993b1b81
Cahya <cahyakumala573@gmail.com>
2023-11-03 18:14:43 +0800

    continue create mobile-navigation(js)

--- Latihan-1/js/script.js
+++ Latihan-1/js/script.js
@@ -1,4 +1,11 @@
 ///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
 // Make mobile navigation work
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
@@ -34,42 +41,35 @@ const obs = new IntersectionObserver(
 );
 obs.observe(sectionProfilEl);
 
-///////////////////////////////////////////////////////////
-// Smooth scrolling animation
+// ///////////////////////////////////////////////////////////
+// // Smooth scrolling animation
 
-const allLinks = document.querySelectorAll("a:link");
-// console.log(allLinks);
+// const allLinks = document.querySelectorAll("a:link");
+// // console.log(allLinks);
 
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    // console.log(e);
-    e.preventDefault();
-    const href = link.getAttribute("href");
-    console.log(href);
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     // console.log(e);
+//     e.preventDefault();
+//     const href = link.getAttribute("href");
+//     console.log(href);
 
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
+//     // Scroll back to top
+//     if (href === "#")
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       });
 
-    //  Scrool to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      console.log(sectionEl);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
+//     //  Scrool to other links
+//     if (href !== "#" && href.startsWith("#")) {
+//       const sectionEl = document.querySelector(href);
+//       console.log(sectionEl);
+//       sectionEl.scrollIntoView({ behavior: "smooth" });
+//     }
 
-    // Close mobile navigation
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
-  });
-});
-
-///////////////////////////////////////////////////////////
-// Set current year
-const yearEl = document.querySelector(".year");
-const currentYear = new Date().getFullYear();
-console.log(currentYear);
-yearEl.textContent = currentYear;
+//     // Close mobile navigation
+//     if (link.classList.contains("main-nav-link"))
+//       headerEl.classList.toggle("nav-open");
+//   });
+// });
